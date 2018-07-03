72311c9be7f4a1ac045c3265326b902f909ae8fb
Prasahnt Kumar <kr.prashant94@gmail.com>
2018-07-03 20:59:19 +0530

    Add files via upload

---
+++ HCSR04/HCSR04.ino
@@ -0,0 +1,14 @@
+#include "LIGHT_HCSR04.h"
+
+
+LIGHT_HCSR04 hcsr04(12, 13);
+
+
+void setup() {
+  hcsr04.begin();
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  Serial.println(hcsr04.distance());
+}
